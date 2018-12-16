8175253ca882fa713b90f8a4f446f57e6e572ce0
Harrison Harnisch <hharnisc@gmail.com>
2018-12-16 12:38:55 -0600

    remove bits copied from plan command

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,36 +1,25 @@
 #!/bin/sh
 set -e
-
 cd "${TF_ACTION_WORKING_DIR:-.}"
 
-WORKSPACE=${TF_ACTION_WORKSPACE:-default}
-terraform workspace select "$WORKSPACE"
-
 set +e
-OUTPUT=$(sh -c "TF_IN_AUTOMATION=true terraform apply -no-color -input=false $*" 2>&1)
+OUTPUT=$(sh -c "terraform apply -no-color -input=false $*" 2>&1)
 SUCCESS=$?
 echo "$OUTPUT"
 set -e
+
+if [ $SUCCESS -eq 0 ]; then
+    exit 0
+fi
 
 if [ "$TF_ACTION_COMMENT" = "1" ] || [ "$TF_ACTION_COMMENT" = "false" ]; then
     exit $SUCCESS
 fi
 
-COMMENT=""
-
-# If not successful, post failed apply output.
-if [ $SUCCESS -ne 0 ]; then
-    COMMENT="#### \`terraform apply\` Failed
+COMMENT="#### \`terraform apply\` Failed
 \`\`\`
 $OUTPUT
 \`\`\`"
-else
-    FMT_PLAN=$(echo "$OUTPUT" | sed -r -e 's/^  \+/\+/g' | sed -r -e 's/^  ~/~/g' | sed -r -e 's/^  -/-/g')
-    COMMENT="\`\`\`diff
-$FMT_PLAN
-\`\`\`"
-fi
-
 PAYLOAD=$(echo '{}' | jq --arg body "$COMMENT" '.body = $body')
 COMMENTS_URL=$(cat /github/workflow/event.json | jq -r .pull_request.comments_url)
 curl -s -S -H "Authorization: token $GITHUB_TOKEN" --header "Content-Type: application/json" --data "$PAYLOAD" "$COMMENTS_URL" > /dev/null
